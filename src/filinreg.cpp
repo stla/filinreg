1d00d12251e004f8e09fc628226151c0d3a73355
stla <laurent_step@yahoo.fr>
2020-11-30 12:35:44 +0100

    cauchy

--- src/filinreg.cpp
+++ src/filinreg.cpp
@@ -26,6 +26,18 @@ double logdnorm(const Eigen::VectorXd& x){
   return -x.cwiseProduct(x).sum() / 2.0;
 }
 
+Eigen::VectorXd qcauchy(const Eigen::VectorXd& p){
+  Eigen::VectorXd out(p.size());
+  for(auto i = 0; i < p.size(); i++){
+    out(i) = tan(M_PI*(p.coeff(i) - 0.5));
+  }
+  return out;
+}
+
+double logdcauchy(const Eigen::VectorXd& x){
+  return -x.cwiseProduct(x).array().log1p().sum();
+}
+
 Eigen::VectorXd qt(const Eigen::VectorXd& p, const double nu){
   boost::math::students_t t(nu);
   Eigen::VectorXd out(p.size());
@@ -74,6 +86,38 @@ Rcpp::List f_normal(
     if(sigma > 0){
       Eigen::VectorXd v = ymI - XmI * theta.topRows(q-1);
       J(counter) = logdnorm(v/sigma) - (n-q) * log(sigma);
+      Theta.col(counter) = theta;
+      counter++;
+    }
+  }
+  return Rcpp::List::create(Rcpp::Named("Theta") = Theta.transpose(),
+                            Rcpp::Named("logWeights") = J);
+}
+
+// [[Rcpp::export]]
+Rcpp::List f_cauchy(
+    const Eigen::MatrixXd& centers,
+    const Eigen::MatrixXd& XI,
+    const Eigen::MatrixXd& XmI,
+    const Eigen::VectorXd& yI,
+    const Eigen::VectorXd& ymI,
+    const size_t M,
+    const size_t n
+){
+  const size_t ncenters = centers.cols();
+  const size_t q = XI.cols() + 1;
+  size_t counter = 0;
+  Eigen::VectorXd J(M);
+  Eigen::MatrixXd Theta(q, M);
+  for(size_t m = 0; m < ncenters; m++){
+    Eigen::MatrixXd H(q,q);
+    H << XI, qcauchy(centers.col(m));
+    Eigen::MatrixXd Ht = H.transpose();
+    Eigen::VectorXd theta = (Ht * H).inverse() * Ht * yI;
+    double sigma = theta.coeff(q-1);
+    if(sigma > 0){
+      Eigen::VectorXd v = ymI - XmI * theta.topRows(q-1);
+      J(counter) = logdcauchy(v/sigma) - (n-q) * log(sigma);
       Theta.col(counter) = theta;
       counter++;
     }
